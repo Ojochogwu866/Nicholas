831de592d362cb17e9be46feec18a6b828729594
Ojochogwu <07afrik@gmail.com>
2023-02-16 11:53:49 +0100

    serv

--- sanity/schemas/Landing.js
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'profileInfo',
-  title: 'profileInfo',
-  type: 'document',
-  fields: [
-    {name: 'name', title: 'Name', type: 'string'},
-    {name: 'title', title: 'Title', type: 'string'},
-  ],
-}
